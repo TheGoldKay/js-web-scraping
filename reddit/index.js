ea45d748f8346028fbe62079e5f1131881642b40
ruby <jhonnatanwfc@live.com>
2023-07-21 22:28:31 -0300

    fixing gpt-4 mistakes

--- reddit/index.js
+++ reddit/index.js
@@ -1,15 +1,19 @@
 const puppeteer = require('puppeteer');
 
 async function run() {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({executablePath: '/usr/bin/google-chrome'});
     const page = await browser.newPage();
     
     await page.goto('https://www.reddit.com/r/javascript/');
     
     const result = await page.evaluate(() => {
-        let title = document.querySelector('h3._eYtD2XCVieq6emjKBH3m').innerText;
-        let url = document.querySelector('a.SQnoC3ObvgnGjWt90zD9Z_').href;
+        let title = document.querySelector('h3._eYtD2XCVieq6emjKBH3m').innerText; // innetText / textContent
+        let url = document.querySelector('a._2INHSNB8V5eaWp4P0rY_mE').href;
         return {title, url};
+    })
+    .catch(() => {
+        console.log('again...');
+        run();
     });
     
     console.log(result);
