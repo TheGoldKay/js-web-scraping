923d5be58d4bd7914bf2f8612f5b675b4aaed377
ruby <jhonnatanwfc@live.com>
2023-08-02 19:40:34 -0300

    testing on one page / one story

--- tgstorytime/index.js
+++ tgstorytime/index.js
@@ -2,21 +2,32 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 async function run() {
+  let novels = [];
+  let page = 0;
   try {
-    const response = await axios.get('https://www.tgstorytime.com/browse.php?type=titles&offset=0');
+    const response = await axios.get(`https://www.tgstorytime.com/browse.php?type=titles&offset=${page}`);
     const html = response.data;
-
     // Load the HTML content into Cheerio
     const $ = cheerio.load(html);
-
     // Select all elements with the class 'listboxtop' and get their text content
     //const data = $('.listbox').map((index, element) => $(element).text()).get();
-    /*
-    const novels = $('.listbox');
-    console.log($($(novels.eq(127)).find('.title')).eq(1).text());
-    */
-    const novels = $('div.listboxtop');
-    console.log(novels.eq(0).text());
+    const storyList = $('div.listboxtop');
+    const first = storyList.eq(0);
+    const titleLink = first.find('div.title').eq(1).find('a').eq(0);
+    const title = titleLink.text();
+    const titleUrl = titleLink.attr('href');
+    const authorLink = first.find('div.title').eq(1).find('a').eq(1);
+    const author = authorLink.text();
+    const authorUrl = authorLink.attr('href');
+    const summary = first.find('div.summarytext').html().replace(/<br>/g, '\n');
+    novels.push({
+      title: title,
+      titleUrl: titleUrl,
+      author: author,
+      authorUrl: authorUrl,
+      summary: summary
+    });
+    console.log(novels)
   } catch (error) {
     console.error('Error:', error.message);
   }
