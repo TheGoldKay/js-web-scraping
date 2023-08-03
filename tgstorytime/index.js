e1e18d7b95a48e50890d2464466f1df74c5ff26c
ruby <jhonnatanwfc@live.com>
2023-08-02 21:02:39 -0300

    saving data

--- tgstorytime/index.js
+++ tgstorytime/index.js
@@ -1,8 +1,9 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
+const fs = require('fs');
 
 async function run() {
-  let novels = [];
+  let novels = {};
   let page = 0;
   try {
     const response = await axios.get(`https://www.tgstorytime.com/browse.php?type=titles&offset=${page}`);
@@ -12,22 +13,29 @@ async function run() {
     // Select all elements with the class 'listboxtop' and get their text content
     //const data = $('.listbox').map((index, element) => $(element).text()).get();
     const storyList = $('div.listboxtop');
-    const first = storyList.eq(0);
-    const titleLink = first.find('div.title').eq(1).find('a').eq(0);
-    const title = titleLink.text();
-    const titleUrl = titleLink.attr('href');
-    const authorLink = first.find('div.title').eq(1).find('a').eq(1);
-    const author = authorLink.text();
-    const authorUrl = authorLink.attr('href');
-    const summary = first.find('div.summarytext').html().replace(/<br>/g, '\n');
-    novels.push({
-      title: title,
-      titleUrl: titleUrl,
-      author: author,
-      authorUrl: authorUrl,
-      summary: summary
+    storyList.each((index, element) => {
+      const story = $(element);
+      const titleLink = story.find('div.title').eq(1).find('a').eq(0);
+      const title = titleLink.text();
+      const titleUrl = titleLink.attr('href');
+      const authorLink = story.find('div.title').eq(1).find('a').eq(1);
+      const author = authorLink.text();
+      const authorUrl = authorLink.attr('href');
+      const summary = story.find('div.summarytext').html().replace(/<br>/g, '\n');
+      novels[title] =  {
+        titleUrl: titleUrl,
+        author: author,
+        authorUrl: authorUrl,
+        summary: summary
+      };
     });
-    console.log(novels)
+    fs.writeFile('tgdata.json', JSON.stringify(novels), (err) => {
+      if(err){
+        console.error('Error: ', err);
+      }else{
+        console.log('Data scrapped successfully');
+      }
+    });
   } catch (error) {
     console.error('Error:', error.message);
   }
